1df93d8d4bf6438d1e97fa25229fa1e5d053d682
Marc Levy <marc.levy.to@me.com>
2015-08-29 18:59:28 -0400

    Add migrations

--- db/migrate/20150829212755_create_company_data.rb
+++ db/migrate/20150829212755_create_company_data.rb
@@ -1,6 +1,16 @@
 class CreateCompanyData < ActiveRecord::Migration
   def change
     create_table :company_data do |t|
+      t.integer :company_id
+
+      t.datetime  :date
+      t.string    :category
+      t.string    :employee_name
+      t.string    :employee_address
+      t.string    :expense_description
+      t.decimal   :pre_tax_amount, precision: 12, scale: 2
+      t.string    :tax_name
+      t.decimal   :tax_amount, precision: 12, scale: 2
 
       t.timestamps null: false
     end
