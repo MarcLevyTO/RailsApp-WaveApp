5ab2f3f52b1a8601b1c0db2a16216d68a301df72
Marc Levy <marc.levy.to@me.com>
2015-08-31 11:55:48 -0400

    Fixed more tests

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -23,13 +23,29 @@ class CompanyTest < ActiveSupport::TestCase
   end
 
   # Create a company, then create a datum and assign it to the company, assert grouped_monthly returns 1 group
-  test "Create two company with one having errors" do
-    original = Company.create(:company_name => 'This is a dup')
-    a = Company.create(:company_name =>  'This is a dup')
-    assert !a.valid?
-    assert a.errors.full_messages.include?("Company name has already been taken")
+  test "Create datum returns one group" do
+    original = Company.create(:company_name => 'Test Company')
+    testDatum = CompanyDatum.create(:company => original, :date => Date.today)
+    assert_not_nil testDatum
+    assert_equal testDatum.company_id, original.id
+
+    assert_equal 1, original.grouped_monthly.length
   end
 
   # Create a company, then create 2 datum with different data data months, assert grouped_monthly returns 2 groups
+  test "Create datum returns two groups" do
+    original = Company.create(:company_name => 'Test Company')
+    testDatum1 = CompanyDatum.create(:company => original, :date => Date.today)
+    assert_not_nil testDatum1
+    assert_equal testDatum1.company_id, original.id
+
+    assert_equal 1, original.grouped_monthly.length
+
+    testDatum2 = CompanyDatum.create(:company => original, :date => (Date.today - 3.months))
+    assert_not_nil testDatum2
+    assert_equal testDatum2.company_id, original.id
+
+    assert_equal 2, original.grouped_monthly.length
+  end
 
 end
